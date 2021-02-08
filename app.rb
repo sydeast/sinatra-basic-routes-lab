eb8532a7e46a4ced5258dcbaecd82dc7e4cfec03
Sydney Daniels <sydeast@gmail.com>
2021-02-08 17:47:10 -0600

    commit tree dbed03723e8ce745b2bc91bc603465b558754368

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        'My name is Syd'
+    end
+
+    get '/hometown' do
+        'My hometown is San Diego'
+    end
+
+    get '/favorite-song' do
+        'My favorite song is Chocolate Love'
+    end
 end
